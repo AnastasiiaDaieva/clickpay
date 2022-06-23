7f8863bdb0aa950df856ddc6a850fad6f4b1d872
eche-unicorn <d.anasteysha@gmail.com>
2022-06-24 02:50:37 +0300

    check

--- src/data/baseUrl.js
+++ src/data/baseUrl.js
@@ -1,3 +1,3 @@
 export const heroku = "https://clickpay-backend.herokuapp.com/api";
 export const localHost = "http://localhost:5000/api";
-export const VPS = "http://153.92.1.11:5000/api";
+export const VPS = "https://153.92.1.11:5000/api";
