8389ad57235071b5e968890aafdae0be659d4501
eche-unicorn <d.anasteysha@gmail.com>
2022-06-24 09:21:00 +0300

    base check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { heroku, localHost, VPS } from "data/baseUrl";
 import axios from "axios";
 
 console.log(window.location.protocol);
-axios.defaults.baseURL = localHost;
+axios.defaults.baseURL = VPS;
 
 const HomepageView = lazy(() =>
   import("views/HomepageView" /*webpackChunkName: "home-view" */)
